520012fab26eb89308fc439927ecce337eda61e8
DelaramGh <deli.zizou@gmail.com>
2024-09-28 17:07:02 -0400

    goal flag added

--- app/goal.py
+++ app/goal.py
@@ -9,35 +9,39 @@ from io import BytesIO
 
 class Goal:
     def get_motivation(self, goal):
-        os.environ['OPENAI_API_KEY'] = open('key.txt').read().strip()
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        client = openai.OpenAI()
-    
-        response = client.completions.create(
-            model="gpt-3.5-turbo-instruct",
-            prompt=f"Provide an inspiring and motivational quote related to the following goal: {goal}",
-            max_tokens=200, n=1, stop=None, temperature=1,
-        )
-        quote = response.choices[0].text.strip()
+        if 0:
+            os.environ['OPENAI_API_KEY'] = open('key.txt').read().strip()
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+            client = openai.OpenAI()
+        
+            response = client.completions.create(
+                model="gpt-3.5-turbo-instruct",
+                prompt=f"Provide an inspiring and motivational quote related to the following goal: {goal}",
+                max_tokens=200, n=1, stop=None, temperature=1,
+            )
+            quote = response.choices[0].text.strip()
 
 
-        # Generate image
-        image_response = client.images.generate(
-            model="dall-e-3",
-            prompt=f"Provide an inspiring and motivational animated image related to the following goal: {goal}. Create the image in a nice workspace with good vibes and nice colors.",
-            size="1024x1024",
-            quality="standard",
-            n=1,
-        )
-        image_url = image_response.data[0].url
+            # Generate image
+            image_response = client.images.generate(
+                model="dall-e-3",
+                prompt=f"Provide an inspiring and motivational animated image related to the following goal: {goal}. Create the image in a nice workspace with good vibes and nice colors.",
+                size="1024x1024",
+                quality="standard",
+                n=1,
+            )
+            image_url = image_response.data[0].url
 
-        # Download the image
-        image_data = requests.get(image_url).content
-        image = Image.open(BytesIO(image_data))
-        # Save and display the image
-        image_filename = "goal_image.png"
-        image_path = os.path.join(os.getcwd(), 'static', 'images', image_filename) 
-        print(image_path)
-        image.save(image_path)
+            # Download the image
+            image_data = requests.get(image_url).content
+            image = Image.open(BytesIO(image_data))
+            # Save and display the image
+            image_filename = "goal_image.png"
+            image_path = os.path.join(os.getcwd(), 'static', 'images', image_filename) 
+            print(image_path)
+            image.save(image_path)
 
+        else:
+            image_filename = "goal_image.png"
+            quote = "Be mindful of the present moment, for in stillness, peace blooms and clarity arises."
         return quote, image_filename
